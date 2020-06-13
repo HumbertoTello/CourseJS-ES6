f5baf61b8156d5cf7d18176e3bfa20cc18663274
Humberto Tello <betoelo2@gmail.com>
2020-06-12 23:33:43 -0300

    Template literals and object short syntax

---
+++ templateLiteralsAndShortSyntax.js
@@ -0,0 +1,12 @@
+const name = 'Humberto'
+const age = '22'
+
+//console.log('Oi, meu nome é ' + name + ' e eu tenho ' + age + ' anos!');
+console.log(`Oi, meu nome é ${name} e eu tenho ${age} anos`);
+
+const user = {
+  name,
+  age
+}
+
+console.log(user);
