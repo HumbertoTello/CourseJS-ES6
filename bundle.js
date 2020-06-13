cde5081896dcbf69f60c3f67ce621275c47611a4
Humberto Tello <betoelo2@gmail.com>
2020-06-12 23:34:25 -0300

    Babel

--- bundle.js
+++ bundle.js
@@ -1,25 +1,11 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var name = 'Humberto';
+var age = '22'; //console.log('Oi, meu nome é ' + name + ' e eu tenho ' + age + ' anos!');
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-alert('Test');
-
-var Test = /*#__PURE__*/function () {
-  function Test() {
-    _classCallCheck(this, Test);
-  }
-
-  _createClass(Test, [{
-    key: "method",
-    value: function method() {}
-  }, {
-    key: "anotherMethod",
-    value: function anotherMethod() {}
-  }]);
-
-  return Test;
-}();
+console.log("Oi, meu nome \xE9 ".concat(name, " e eu tenho ").concat(age, " anos"));
+var user = {
+  name: name,
+  age: age
+};
+console.log(user);
